3ef7384110a984ace43767dd3648d730f5afb25f
Snoumann <56759085+Snoumann@users.noreply.github.com>
2019-10-20 23:41:57 +0300

    Initial commit

---
+++ src/Oper.java
@@ -0,0 +1,13 @@
+public class Oper {
+    int a;
+    int b;
+
+    public float sum() {
+        return a+b;
+    }
+
+    public int dif() {
+        return a-b;
+    }
+
+}
---
+++ src/Probal.java
@@ -0,0 +1,10 @@
+public class Probal {
+
+    public static void main(String[] args) {
+        Oper op = new Oper();
+        op.a = 6;
+        op.b = 7;
+        System.out.println("Сумма=" + op.sum());
+        System.out.println("Разность=" + op.dif());
+    }
+}
